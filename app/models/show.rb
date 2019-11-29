aba384636e2b0a4eae1f2e0bf482668a1a3b845b
Zachary Schulz <zjschulz@gmail.com>
2019-11-29 15:34:02 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -13,7 +13,7 @@ class Show < ActiveRecord::Base
   end 
   
   def self.least_popular_show
-    where("rating = ?", lowest_rating
+    where("rating = ?", lowest_rating)
   end
   
   def self.ratings_sum
