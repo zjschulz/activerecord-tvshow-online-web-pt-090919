79a95d4619c19eb7c71a6fafa7fed4e61acfdfaa
Zachary Schulz <zjschulz@gmail.com>
2019-11-29 15:13:16 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,17 @@
 class Show < ActiveRecord::Base
   
+  def highest_rating
+  end
+  
+  def most_popular_show 
+  end
+  
+  def lowest_rating 
+  end 
+  
+  def least_popular_show 
+  end
+  
   
   
 end
