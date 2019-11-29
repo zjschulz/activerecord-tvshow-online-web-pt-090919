b5757dc366cc71b75be97c9892075ce2c93896a8
Zachary Schulz <zjschulz@gmail.com>
2019-11-29 15:27:21 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    self.where("rating = ?", self.highest_rating)
+    Show.where("rating = ?", highest_rating)
   end
   
   def self.lowest_rating
