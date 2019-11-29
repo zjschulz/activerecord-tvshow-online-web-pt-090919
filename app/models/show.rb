d77924bd586744950493ffdcf450128c187edec6
Zachary Schulz <zjschulz@gmail.com>
2019-11-29 15:19:02 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,7 +7,8 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
   end
   
-  def self.lowest_rating 
+  def self.lowest_rating
+    Show.minimum(:rating)
   end 
   
   def self.least_popular_show 
