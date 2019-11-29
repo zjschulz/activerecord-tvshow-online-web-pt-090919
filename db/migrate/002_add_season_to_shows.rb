82e71e08185d9ab0ff6dd5c47bcb8ad1ad10f478
Zachary Schulz <zjschulz@gmail.com>
2019-11-29 15:06:40 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,8 +1,7 @@
-class AddSeasonToShows < ActiveRecord::Migration[5.1]
+class AddSeasonToShows < ActiveRecord::Migration[5.2]
   
   def change
-    add_column(:students, :grade, :integer)
-    add_column(:students, :birthdate, :string)
+    add_column(:shows, :season, :string)
   end
   
 end
